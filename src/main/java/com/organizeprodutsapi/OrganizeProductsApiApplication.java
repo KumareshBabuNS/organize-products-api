c2c06259c994a42540dd415c7439bb296397238d
Dadonas <dadonas@gmail.com>
2017-10-11 21:54:30 -0300

    Initial Commit

---
+++ src/main/java/com/organizeprodutsapi/OrganizeProductsApiApplication.java
@@ -0,0 +1,12 @@
+package com.organizeprodutsapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrganizeProductsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrganizeProductsApiApplication.class, args);
+	}
+}
